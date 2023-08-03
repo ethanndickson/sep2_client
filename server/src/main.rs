8be1d64b39b950a3cdd7e69937aa5fbccb410fb1
Ethan Dickson <ethanndickson@gmail.com>
2023-08-03 23:53:49 +1000

    refactor: use a common server for notifs & testing

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,14 @@
-use clap::Parser;
-use log::info;
-use server::Server;
-use std::error::Error;
+use std::sync::Arc;
 
-mod server;
-mod tls;
+use anyhow::{Ok, Result};
+use async_trait::async_trait;
+use clap::Parser;
+use common::packages::xsd::{DeviceCapability, EndDevice};
+use common::serialize;
+use common::server::{ClientNotifServer, NotifHandler};
+use hyper::{Body, Method, StatusCode};
+use hyper::{Request, Response};
+use log::info;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -18,10 +22,35 @@ struct Args {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn main() -> Result<()> {
     let args = Args::parse();
     let addr = format!("127.0.0.1:{}", args.port);
-    let server = Server::new(&addr, &args.cert, &args.key).unwrap();
+    let handler = TestHandler {};
+    let server = ClientNotifServer::new(&addr, &args.cert, &args.key, handler).unwrap();
     info!("Server listening on {addr}");
     server.run().await
+}
+
+pub struct TestHandler {}
+
+#[async_trait]
+impl NotifHandler for TestHandler {
+    async fn notif_handler(&self, name: &str, resource: &str) -> Result<()> {
+        Ok(())
+    }
+    async fn request_service(self: Arc<Self>, req: Request<Body>) -> Result<Response<Body>> {
+        let mut response = Response::new(Body::empty());
+        match (req.method(), req.uri().path()) {
+            (&Method::GET, "/dcap") => {
+                *response.body_mut() = Body::from(serialize(DeviceCapability::default()));
+            }
+            (&Method::GET, "/edev/1/") => {
+                *response.body_mut() = Body::from(serialize(EndDevice::default()));
+            }
+            _ => {
+                *response.status_mut() = StatusCode::NOT_FOUND;
+            }
+        };
+        Ok(response)
+    }
 }
