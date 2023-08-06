348cbd83b6d5d175caf7a9555bbdd4aa8e3f65b8
Ethan Dickson <ethanndickson@gmail.com>
2023-08-06 18:10:23 +1000

    feat: client put, post & delete

--- client/src/client.rs
+++ client/src/client.rs
@@ -43,12 +43,45 @@ impl Client {
     }
 
     pub async fn post<R: SEResource>(&self, path: &str, resource: &R) -> Result<()> {
+        self.put_post(path, resource, Method::POST).await
+    }
+
+    pub async fn delete(&self, path: &str) -> Result<()> {
+        let uri: Uri = format!("https://{}{}", self.addr, path).parse()?;
+        info!("DELETE at {}", uri);
+        let req = Request::builder()
+            .method(Method::DELETE)
+            .uri(uri)
+            .body(Body::empty())?;
+        debug!("Outgoing HTTP Request: {:?}", req);
+        let res = self.http.request(req).await?;
+        debug!("Incoming HTTP Response: {:?}", res);
+        // TODO: Improve erorr handling
+        match res.status() {
+            StatusCode::NO_CONTENT => Ok(()),
+            StatusCode::BAD_REQUEST => bail!("400 Bad Request"),
+            StatusCode::NOT_FOUND => bail!("404 Not Found"),
+            _ => bail!("Unexpected HTTP response from server"),
+        }
+    }
+
+    pub async fn put<R: SEResource>(&self, path: &str, resource: &R) -> Result<()> {
+        self.put_post(path, resource, Method::PUT).await
+    }
+
+    // Create a PUT or POST request
+    async fn put_post<R: SEResource>(
+        &self,
+        path: &str,
+        resource: &R,
+        method: Method,
+    ) -> Result<()> {
         let uri: Uri = format!("https://{}{}", self.addr, path).parse()?;
         info!("POST {} to {}", R::name(), uri);
         let rsrce = serialize(resource)?;
         let rsrce_size = rsrce.as_bytes().len();
         let req = Request::builder()
-            .method(Method::POST)
+            .method(method)
             .header(CONTENT_TYPE, "application/sep+xml")
             .header(CONTENT_LENGTH, rsrce_size)
             .uri(uri)
