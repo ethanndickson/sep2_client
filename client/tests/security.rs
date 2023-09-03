7266a8077b05abb25127cf6ef0ae7d919b245017
Ethan Dickson <ethanndickson@gmail.com>
2023-09-03 16:27:26 +1000

    test: security fs tests

---
+++ client/tests/security.rs
@@ -0,0 +1,18 @@
+use client::security::{security_init, sfdi_gen};
+use common::packages::primitives::HexBinary160;
+
+#[test]
+fn security_generates() {
+    let _ = security_init("../certs/client_cert.pem").unwrap();
+}
+
+/// Generating SFDI as per the specification
+#[test]
+fn example_sfdi_gen() {
+    let lfdi = HexBinary160([
+        0x3E, 0x4F, 0x45, 0xAB, 0x31, 0xED, 0xFE, 0x5B, 0x67, 0xE3, 0x43, 0xE5, 0xE4, 0x56, 0x2E,
+        0x31, 0x98, 0x4E, 0x23, 0xE5,
+    ]);
+    let sfdi = sfdi_gen(&lfdi);
+    assert_eq!(sfdi.get(), 167261211391)
+}
